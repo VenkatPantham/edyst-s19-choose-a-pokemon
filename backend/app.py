e63b8ba6a0767ad1da049a290038f3f5a8b7962b
VenkatPantham <venkatpantham5568@gmail.com>
2019-04-14 03:04:46 +0530

    done with backend

---
+++ backend/app.py
@@ -0,0 +1,11 @@
+from flask import Flask,jsonify
+
+app=Flask(__name__)
+
+@app.route('/api/pokemon')
+def pokemon():
+    pokemon=['bulbasaur','charmander','squirtle']
+    return jsonify(pokemon)
+
+if(__name__=='__main__'):
+    app.run(host='localhost',port=8006,debug=True)
