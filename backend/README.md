80e839f5ea3f8ba7c206abb5662772adc1d4dace
VenkatPantham <venkatpantham5568@gmail.com>
2019-04-21 16:04:04 +0530

    resolved some issues

---
+++ backend/README.md
@@ -0,0 +1,9 @@
+# API service for Pokemon data
+The backend folder consists of app.py,requirements.txt
+
+app.py:
+    This file is an used as API service which is used to display the pokemon data in JSON Fomrat
+
+requirements.txt:
+    This file consists of all the requirements which are used by the API service
+    All these requriements should be installed in order to run the application
---
+++ backend/app.py
@@ -0,0 +1,16 @@
+from flask import Flask,jsonify,render_template
+
+app=Flask(__name__)                                                 # Initialising Flask
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
+
+@app.route('/api/pokemon')
+def pokemon():
+    data={}
+    data['pokemon']=["bulbasaur","charmander","squirtle"]           # Storing data into a variable
+    return jsonify(data)                                            # Returning required JSON Data
+
+if(__name__=='__main__'):
+    app.run(host='localhost',port=8006)
