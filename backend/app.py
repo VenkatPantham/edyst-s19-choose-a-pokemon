5c7efcbe26efe9b07699496e2c495204d027ee10
Venkat Pantham <venkatpantham5568@gmail.com>
2019-04-21 17:04:22 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask,json,render_template
 
 app=Flask(__name__)                                                 # Initialising Flask
 
-@app.errorhandler(404)
+@app.errorhandler(404)                                              # Handling 404 error
 def page_not_found(e):
     return render_template('404.html'),404
 
@@ -10,7 +10,7 @@ def page_not_found(e):
 def pokemon():
     data={}
     data['pokemon']=["bulbasaur","charmander","squirtle"]           # Storing data into a variable
-    return json.dumps(data)                                            # Returning required JSON Data
+    return json.dumps(data)                                         # Returning required JSON Data
 
 if(__name__=='__main__'):
     app.run(host='localhost',port=8006)
